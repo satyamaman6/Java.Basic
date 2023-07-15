18672497048404514f9aa17ce856031353409961
Satyam Shrivastava <111362821+satyamaman6@users.noreply.github.com>
2023-07-15 15:24:40 +0530

    Duplicate.java

--- Duplicate.java
+++ Duplicate.java
@@ -21,5 +21,19 @@ public class Duplicate
                 }
             }
         }
+    
+        String str = new String("ANFJFJUTIOK");
+        int count = 0;
+        char[] chars =str.toCharArray();
+        System.out.println("Duplicate character are:");
+        for (int i=0; i<str.length();i++){
+            for (int j=i+1;j<str.length();j++) {
+                if (chars[i] == chars[j]) {
+                    System.out.println(chars[j]);
+                    count++;
+                    break;
+                }
+            }
+        }
     }
 }
