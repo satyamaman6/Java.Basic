19892753afe67eb95005608de854df333b2bcc19
Satyam Shrivastava <111362821+satyamaman6@users.noreply.github.com>
2023-07-15 15:30:56 +0530

    Add files via upload
    for loop and while loop

---
+++ loops.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class loops
+{
+    //Use for loop to find the number upto the specific place
+    public static void main(String[] args)
+    {
+        int num;
+         for (num = 0; num <= 100; num+=2)
+              {
+                  System.out.println(num);
+              }
+//======================================================================================================================
+        //Print the value with the number difference -4
+        int n;
+         for (n =100 ; n>=0; n-= 3)
+         {
+             System.out.print(n + " ");
+         }
+
+    // Using While try to go through the loop upto Infinite
+
+
+        while ( true)
+        {
+            System.out.println("Satyam Aman Verma");
+        }
+    }
+}
